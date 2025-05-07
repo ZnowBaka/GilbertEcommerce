300d587307a01438994521044bb438ab7c1e9575
ZnowBaka <Snorrebr@gmail.com>
2025-05-07 11:38:07 +0200

    Initial commit

---
+++ src/main/java/com/example/gilbertecommerce/GilbertEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.gilbertecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GilbertEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GilbertEcommerceApplication.class, args);
+    }
+
+}
