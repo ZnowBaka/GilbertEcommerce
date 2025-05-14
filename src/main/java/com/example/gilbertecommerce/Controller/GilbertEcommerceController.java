f7a9eb2795279d6a071676e8d8af73b073f0064e
Deverver <jackRMrasmussen@gmail.com>
2025-05-14 14:58:31 +0200

    initial commit

--- src/main/java/com/example/gilbertecommerce/Controller/GilbertEcommerceController.java
+++ src/main/java/com/example/gilbertecommerce/Controller/GilbertEcommerceController.java
@@ -1,9 +1,6 @@
 package com.example.gilbertecommerce.Controller;
 
-import com.example.gilbertecommerce.CustomException.IncorrectPasswordException;
-import com.example.gilbertecommerce.Entity.LoginInfo;
 import com.example.gilbertecommerce.Entity.RegistrationForm;
-import com.example.gilbertecommerce.Entity.User;
 import com.example.gilbertecommerce.Service.LoginService;
 import com.example.gilbertecommerce.Service.UserService;
 import jakarta.servlet.http.HttpSession;
@@ -14,13 +11,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class LoginController {
+public class GilbertEcommerceController {
 
     private final LoginService loginService;
     private final UserService userService;
     private final HttpSession session;
 
-    public LoginController(LoginService loginService, UserService userService, HttpSession session) {
+    public GilbertEcommerceController(LoginService loginService, UserService userService, HttpSession session) {
         this.loginService = loginService;
         this.userService = userService;
         this.session = session;
@@ -28,12 +25,28 @@ public class LoginController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("registrationFrom", new RegistrationForm());
-        return "redirect:/registerNewProfile";
+
+        return "redirect:/welcomePage";
     }
+
+
+    @GetMapping("/welcomePage")
+    public String welcome(Model model) {
+
+        return "welcomePage";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage(Model model) {
+
+        return "/loginPage";
+    }
+
 
     @PostMapping("/registerNewProfile")
     public String postNewProfile(@ModelAttribute("registrationForm") RegistrationForm registrationForm, Model model) {
+        model.addAttribute("registrationFrom", new RegistrationForm());
+
         if (!loginService.doesLoginInfoExist(registrationForm.getLoginInfo().getLoginName())) {
             loginService.registerUser(registrationForm.getLoginInfo(), registrationForm.getUser());
             return "redirect:/login";
@@ -43,25 +56,29 @@ public class LoginController {
         }
     }
 }
-//    @GetMapping("/loginPage")
-//    public String loginPage(Model model) {
-//        session.invalidate();
-//        model.addAttribute("user", new User());
-//        return "/loginPage";
-//    }
-//
-//    @PostMapping("/loginPage")
-//    public String postLoginPage(@ModelAttribute("user") User user, Model model) throws IncorrectPasswordException {
-//        User loggedInUser = userService.loginUser(user);
-//        if (loggedInUser != null) {
-//
-//            session.setAttribute("user", loggedInUser);
-//            session.setAttribute("profile", profileService.getProfileById((User) session.getAttribute("user")));
-//            return "redirect:/frontPage";
-//        } else if (loggedInUser == null) {
-//            model.addAttribute("error", "Incorrect username or password");
-//            return "/loginPage";
-//        }
-//        return "/loginPage";
-//    }
-//}
+/*
+    @GetMapping("/loginPage")
+    public String loginPage(Model model) {
+        session.invalidate();
+        model.addAttribute("user", new User());
+        return "/loginPage";
+    }
+
+    @PostMapping("/loginPage")
+    public String postLoginPage(@ModelAttribute("user") User user, Model model) throws IncorrectPasswordException {
+        User loggedInUser = userService.loginUser(user);
+        if (loggedInUser != null) {
+
+            session.setAttribute("user", loggedInUser);
+            session.setAttribute("profile", profileService.getProfileById((User) session.getAttribute("user")));
+            return "redirect:/frontPage";
+        } else if (loggedInUser == null) {
+            model.addAttribute("error", "Incorrect username or password");
+            return "/loginPage";
+        }
+        return "/loginPage";
+    }
+}
+ */
+
+
